744a4173e4c8e530470859ad716f995b9bc62269
Smit <smit.kukade.tech@gmail.com>
2026-09-23 15:38:33 +0530

    Add lesson 10 exercise solutions

---
+++ javascript-exercise-solutions/lesson-10/scripts/10j.js
@@ -0,0 +1,11 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+function updateCalculation(value) {
+    calculation += value;
+    displayCalculation();
+    localStorage.setItem('calculation', calculation);
+}
+
+function displayCalculation() {
+    document.querySelector('.js-calculation').innerHTML = calculation;
+}
